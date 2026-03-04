5499a6ffd3c6afb10d2a5a0fd2a7b84d69dcc0c7
Shrek68 <sandesh1234poudels@gmail.com>
2026-03-04 13:09:13 +0545

    Modulo magic

--- src/Driver.c
+++ src/Driver.c
@@ -93,10 +93,9 @@ void mouse_apply(Mouse* rat)
         sendPayload(rat->handle, packet);
     }
 
-    if (rat->current_profile_index == 0)
-        rat->current_profile_index = 1;
+    rat->current_profile_index = (((rat->current_profile_index - 1) % 6) + 6) % 6 + 1;
 
-    if (rat->current_profile_index == 0) rat->current_profile_index = 1;
     build_dpi_payload(packet, rat->current_profile_index, rat->profiles[rat->current_profile_index - 1].dpi_value, active_mask);
+
     sendPayload(rat->handle, packet);
 }
